6e26ab6a7ae2a4f144694a10d403be1a6af766a3
vasilis <bill2005.4@gmail.com>
2025-01-13 01:56:43 +0200

    platforms with collision

---
+++ include/platform.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "gameobject.h"
+#include "box.h"
+#include "sgg/graphics.h"
+#include <vector>
+
+class Platform : public GameObject
+{
+	std::vector<std::string> platform_names;
+	std::vector<Box> platforms;
+	std::string image_name;
+	float pos_x;
+	float pos_y;
+	float height;
+	float width;
+
+	graphics::Brush platform_brush;
+	graphics::Brush platform_brush_debug;
+public:
+	Platform() = default; //first platform data used
+	virtual ~Platform() = default; // destructor
+
+	void platformInitHandler();
+	void drawPlatform(int i);
+	void platformDisplayHandler();
+	void addPlatform(float x, float y, float w, float h, std::string IN);
+	std::vector<Box> getPlatforms();
+};
