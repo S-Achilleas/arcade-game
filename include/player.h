027044020d103814cf26bcffd27378c11dd4e286
BILL <bill2005.4@gmail.com>
2025-01-14 15:12:36 +0200

    removed unused drawBlock function

--- include/player.h
+++ include/player.h
@@ -12,8 +12,6 @@ class Player:public ObjectWithMovement
     int neg = 1;
     float initial_y;
 
-    bool jumping = false;
-    bool coll_up = false;
     std::vector<std::string>jump_array_right;
     std::vector<std::string>jump_array_left;
     std::vector<std::string>idle_array;
@@ -26,7 +24,4 @@ public:
     void draw() override;
     float getPlayerX() {return m_pos_x;}
     float getPlayerY() { return m_pos_y; }
-    bool getJumping(){return jumping;}
-    void setCollJumpe(bool f){coll_up = f;}
-
 };
