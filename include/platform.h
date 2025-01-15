e6b15796ba3b12ef39cb6188621ece9206f0fa28
BILL <bill2005.4@gmail.com>
2025-01-16 00:26:35 +0200

    Syntax Platform(float mpos_x, float mpos_y, float mwidth,   float mheight, std::string IN, float offset_x = 0.0f,   float offset_y = 0.0f, float offset_width = 0.0f, float offset_height = 0.0f);

--- include/platform.h
+++ include/platform.h
@@ -4,25 +4,33 @@
 #include "sgg/graphics.h"
 #include <vector>
 
-class Platform : public GameObject
+class Platform : public GameObject, public Box
 {
-	std::vector<std::string> platform_names;
-	std::vector<Box> platforms;
+	static std::vector<Platform> platforms;
 	std::string image_name;
 	float pos_x = 0.0f;
 	float pos_y = 0.0f;
 	float height = 0.0f;
 	float width = 0.0f;
-	void platformInitHandler();
-	void drawPlatform(int i);
 
-	graphics::Brush platform_brush;
-	graphics::Brush platform_brush_debug;
+	float offset_x;
+	float offset_y;
+	float height_offset;
+	float width_offset;
+
+	static void drawPlatform(int i);
+
+	static graphics::Brush platform_brush;
+	static graphics::Brush platform_brush_debug;
+
 public:
-	Platform() = default; // default constructor
+	Platform(float mpos_x, float mpos_y, float mwidth,
+		float mheight, std::string IN, float offset_x = 0.0f,
+		float offset_y = 0.0f, float offset_width = 0.0f, float offset_height = 0.0f);
 	virtual ~Platform() = default; // destructor
+	static void platformInitHandler(Platform platform);
+	static void platformBrushInit();
 
-	void platformDisplayHandler();
-	void addPlatform(float x, float y, float w, float h, std::string IN);
-	std::vector<Box> getPlatforms();
+	void static platformDisplayHandler();
+	std::vector<Platform> getPlatforms();
 };
