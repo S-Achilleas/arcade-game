35227250bfb7a1e17ce470f368ac2720cd7e5fdd
BILL <bill2005.4@gmail.com>
2025-01-16 01:36:44 +0200

    pause can now return to menu

--- source/gamestate.cpp
+++ source/gamestate.cpp
@@ -52,12 +52,21 @@ void GameState::update(float dt) {
     if (dt > 500) return;
 
     if (graphics::getKeyState(graphics::SCANCODE_P) 
-        && float(pauseTimer)==1 && menu_skipped){
+        && float(pauseTimer)==1 && menu_skipped)
+    {
         pauseTimer.start();
         if (game_paused) { game_paused = false; pause_brush.fill_opacity = 0.0f; }
         else { game_paused = true; pause_brush.fill_opacity = 1.0f; }
     }
-    if (game_paused) return;
+    if (game_paused) 
+    { 
+        if (graphics::getKeyState(graphics::SCANCODE_M)) {
+            game_paused = false;
+            menu_skipped = false;
+            GameState::init();
+        }
+        return; 
+    }
 
     float sleep_time = std::max(0.0f, 17.0f - dt);
 
@@ -78,6 +87,8 @@ void GameState::draw() {
     { 
         graphics::drawText(2.0f, getCanvasHeight() / 2.0f - 1.0f, 1.0f,
             "Game is on Pause", pause_brush);
+        graphics::drawText(1.0f, getCanvasHeight() / 2.0f + 2.0f, 0.7f,
+            "Press M to return to main menu", pause_brush);
         return; //remove this if you want continue drawing on pause
     }
     current_level->draw();
