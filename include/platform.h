9a44a30528d85f6a03aeadf9f3d784bd5be659d0
BILL <bill2005.4@gmail.com>
2025-01-16 13:38:03 +0200

    platform object usage in its vector

--- include/platform.h
+++ include/platform.h
@@ -20,6 +20,12 @@ class Platform : public GameObject, public Box
 	static graphics::Brush platform_brush_debug;
 
 public:
+	/*
+	To make a platform use:
+	Platform::platformInitHandler(Platform(float image_pos_x, float image_pos_y, 
+		float image_width, float image_height, string png, float image_x_offset, 
+		float image_y_offset, float image_width_offset, float image_height_offset));
+	*/
 	Platform(float mpos_x, float mpos_y, float mwidth,
 		float mheight, std::string IN, float offset_x = 0.0f,
 		float offset_y = 0.0f, float offset_width = 0.0f, float offset_height = 0.0f);
