d39a98aa9c20f8b6184508a903c1aded82cffa71
S-Achilleas <sarraxilleas@gmail.com>
2025-01-16 08:43:13 +0200

    Updated the level::update method to choose from all 3 types of enemies. Yet there is a problem with goblin and flyigneye class

---
+++ source/goblin.cpp
@@ -0,0 +1,25 @@
+#include "goblin.h"
+#include "gamestate.h"
+#include "animation.h"
+
+Goblin::Goblin(bool r) : Enemy(r) {
+    init();
+}
+
+void Goblin::init() {
+    my_brush.fill_opacity = 1.0f;
+    my_brush.outline_opacity = 0.0f;
+    d_width = 3.5f;
+    d_height = 3.5f;
+    Enemy::init();
+    my_animation =new Animation(false,graphics::preloadBitmaps(state->getFullAssetPath("goblin/run/right")),
+        graphics::preloadBitmaps(state->getFullAssetPath("goblin/run/left")));
+}
+
+void Goblin::update(float dt) {
+    Enemy::update(dt);
+}
+
+void Goblin::draw() {
+    Enemy::draw();
+}
